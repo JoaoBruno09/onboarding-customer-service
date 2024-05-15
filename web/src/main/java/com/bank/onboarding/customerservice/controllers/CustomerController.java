fd31aa95a0c1817072eea43afcc0d97fc79b02ba
João Rocha <1220256@isep.ipp.pt>
2024-05-16 00:07:03 +0100

    Reorganized project

--- web/src/main/java/com/bank/onboarding/customerservice/controllers/CustomerController.java
+++ web/src/main/java/com/bank/onboarding/customerservice/controllers/CustomerController.java
@@ -3,9 +3,9 @@ package com.bank.onboarding.customerservice.controllers;
 import com.bank.onboarding.commonslib.persistence.models.Address;
 import com.bank.onboarding.commonslib.persistence.models.Contact;
 import com.bank.onboarding.commonslib.persistence.models.Customer;
-import com.bank.onboarding.commonslib.persistence.services.AddressService;
-import com.bank.onboarding.commonslib.persistence.services.ContactService;
-import com.bank.onboarding.commonslib.persistence.services.CustomerService;
+import com.bank.onboarding.commonslib.persistence.services.AddressRepoService;
+import com.bank.onboarding.commonslib.persistence.services.ContactRepoService;
+import com.bank.onboarding.commonslib.persistence.services.CustomerRepoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,22 +17,22 @@ import java.util.List;
 @RequestMapping("customer")
 @RequiredArgsConstructor
 public class CustomerController {
-    private final CustomerService customerService;
-    private final ContactService contactService;
-    private final AddressService addressService;
+    private final CustomerRepoService customerRepoService;
+    private final ContactRepoService contactRepoService;
+    private final AddressRepoService addressRepoService;
 
     @GetMapping("/test/customers")
     public List<Customer> getCustomers() {
-        return customerService.getAllCustomers();
+        return customerRepoService.getAllCustomers();
     }
 
     @GetMapping("/test/contacts")
     public List<Contact> getContacts() {
-        return contactService.getAllContacts();
+        return contactRepoService.getAllContacts();
     }
 
     @GetMapping("/test/addresses")
     public List<Address> getAddresses() {
-        return addressService.getAllAddresses();
+        return addressRepoService.getAllAddresses();
     }
 }
