c306dd0c5c2f166143447e1f3b4d6267d4e1378d
João Rocha <1220256@isep.ipp.pt>
2024-05-28 18:43:48 +0100

    Fixed customer service consumer

--- services/src/main/java/com/bank/onboarding/customerservice/services/KafkaConsumer.java
+++ services/src/main/java/com/bank/onboarding/customerservice/services/KafkaConsumer.java
@@ -1,8 +1,12 @@
 package com.bank.onboarding.customerservice.services;
 
 import com.bank.onboarding.commonslib.utils.kafka.CreateAccountEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +14,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class KafkaConsumer {
-    @KafkaListener(topics = "${spring.kafka.consumer.account.topic-name}",  groupId = "${spring.kafka.consumer.group-id}")
-    public void consumeEvent(Object event){
-        if (event instanceof CreateAccountEvent) {
-            log.info("Event received is " + event);
+
+    private final ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "${spring.kafka.consumer.topic-name}",  groupId = "${spring.kafka.consumer.group-id}")
+    public void consumeEvent(ConsumerRecord event){
+        try {
+            objectMapper.registerModule(new JavaTimeModule());
+            String eventType = objectMapper.readTree(event.value().toString()).asText();
+            CreateAccountEvent createAccountEvent = objectMapper.readValue(eventType, CreateAccountEvent.class);
+            log.info("Event received is " + createAccountEvent);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 }
