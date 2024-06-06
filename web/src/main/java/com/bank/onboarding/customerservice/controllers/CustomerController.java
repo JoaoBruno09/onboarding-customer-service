a78132b3ea15d5e1c3e4b031982cffe07efadccd
JoaoBruno1220256 <1220256@isep.ipp.pt>
2024-06-06 23:48:03 +0100

    Added validatons to DTOS

--- web/src/main/java/com/bank/onboarding/customerservice/controllers/CustomerController.java
+++ web/src/main/java/com/bank/onboarding/customerservice/controllers/CustomerController.java
@@ -6,6 +6,7 @@ import com.bank.onboarding.commonslib.web.dtos.customer.CreateRelationDTO;
 import com.bank.onboarding.commonslib.web.dtos.customer.CustomerDTO;
 import com.bank.onboarding.commonslib.web.dtos.customer.UpdateCustomerRequestDTO;
 import com.bank.onboarding.customerservice.services.CustomerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class CustomerController {
 
     @PutMapping("/{customerNumber}")
     public ResponseEntity<?> updateCustomer(@PathVariable("customerNumber") String customerNumber,
-                                       @RequestBody UpdateCustomerRequestDTO updateCustomerRequestDTO){
+                                            @RequestBody @Valid UpdateCustomerRequestDTO updateCustomerRequestDTO){
         try {
             final CustomerDTO customerDTO = customerService.updateCustomer(customerNumber, updateCustomerRequestDTO);
             return new ResponseEntity<>(customerDTO, HttpStatus.OK);
@@ -36,7 +37,7 @@ public class CustomerController {
 
     @PutMapping("/intervention")
     public ResponseEntity<?> createCustomerIntervenient(@RequestParam(name = "customerNumber", required = false) String customerNumber,
-                                                        @RequestBody CreateIntervenientDTO createIntervenientDTO){
+                                                        @RequestBody @Valid CreateIntervenientDTO createIntervenientDTO){
         try {
             final CustomerDTO customerDTO = customerService.createIntervenientOrAddIntervention(customerNumber, createIntervenientDTO);
             return new ResponseEntity<>(customerDTO, HttpStatus.OK);
@@ -48,7 +49,7 @@ public class CustomerController {
 
     @PutMapping("/relation")
     public ResponseEntity<?> createCustomerRelation(@RequestParam(name = "customerNumber", required = false) String parentCustomerNumber,
-                                                    @RequestBody CreateRelationDTO createRelationDTO){
+                                                    @RequestBody @Valid CreateRelationDTO createRelationDTO){
         try {
             final CustomerDTO customerDTO = customerService.createRelationOrAddRelation(parentCustomerNumber, createRelationDTO);
             return new ResponseEntity<>(customerDTO, HttpStatus.OK);
